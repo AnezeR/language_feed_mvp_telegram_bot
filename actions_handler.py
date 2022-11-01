ce3184f5945a8acc20c46f3e805b2689031c487c
Doukana <31391185+AnezeR@users.noreply.github.com>
2022-11-01 13:44:27 +0300

    changed todo

--- actions_handler.py
+++ actions_handler.py
@@ -63,7 +63,7 @@ def buttons_and_text_messages_handling(message: telebot.types.Message):
 
 bot.infinity_polling()
 
-# todo: give random content to users with daily feed
+# todo: fix text for giving random content with daily feed
 # todo: add translation from russian to english
 # todo: create questionnaires
 # todo: improve landing
