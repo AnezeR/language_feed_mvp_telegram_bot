697db10d625ebeb6bc08f56f4b7700dab4eeb77a
Doukana <31391185+AnezeR@users.noreply.github.com>
2022-11-01 14:44:31 +0300

    added strings to content layouts

--- actions_handler.py
+++ actions_handler.py
@@ -63,7 +63,6 @@ def buttons_and_text_messages_handling(message: telebot.types.Message):
 
 bot.infinity_polling()
 
-# todo: fix text for giving random content with daily feed
 # todo: create questionnaires
 # todo: improve landing
 # todo: switch database to sqlite and merge preloading with database
