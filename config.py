bbf56fb96fde6c9b1be0fd5681bfc89bd68dc834
Doukana <31391185+AnezeR@users.noreply.github.com>
2022-11-02 12:55:24 +0300

    removing unnecessary config

--- config.py
+++ config.py
@@ -8,8 +8,4 @@ class Config:
         self.telegram_bot_api_key = config_file['telegram_bot_api_key']
         self.admin_chat_id = config_file['admin_chat_id']
 
-        self.mariadb_user = config_file['mariadb_user']
-        self.mariadb_password = config_file['mariadb_password']
-        self.mariadb_database = config_file['mariadb_database']
-
         self.current_content = config_file['current_content']
